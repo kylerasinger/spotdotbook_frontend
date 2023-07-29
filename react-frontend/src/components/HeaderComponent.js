3bfa04ae35e62d0688b29d6a9aa81fe6e498e62e
kylerasinger <kyle.rasinger@gmail.com>
2023-07-29 16:14:15 -0400

    Adding Github Actions 9

--- react-frontend/src/components/HeaderComponent.js
+++ react-frontend/src/components/HeaderComponent.js
@@ -14,7 +14,7 @@ class HeaderComponent extends Component {
             <div>
                 <header>
                     <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-                    <div><a href="localhost:3000" className="navbar-brand"> spotdotbook | v0.1.2 </a></div>    
+                    <div><a href="localhost:3000" className="navbar-brand"> spotdotbook | v0.1 </a></div>    
                     </nav>
                 </header>
             </div>
